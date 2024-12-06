8ea5aea4d78f4a6fa8189623d8778e9f70324fd2
PhilipDeFraties <philip.defraties@gmail.com>
2024-12-06 15:41:50 -0700

    update Product model spec expect statements to use is_expected as per rubocop

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'validations' do
-    it { should validate_presence_of(:name) }
-    it { should validate_uniqueness_of(:name) }
-    it { should validate_presence_of(:description) }
-    it { should validate_presence_of(:price) }
-    it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
-    it { should validate_presence_of(:stock_quantity) }
-    it { should validate_numericality_of(:stock_quantity).only_integer.is_greater_than_or_equal_to(0) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:price) }
+    it { is_expected.to validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
+    it { is_expected.to validate_presence_of(:stock_quantity) }
+    it { is_expected.to validate_numericality_of(:stock_quantity).only_integer.is_greater_than_or_equal_to(0) }
   end
 end
