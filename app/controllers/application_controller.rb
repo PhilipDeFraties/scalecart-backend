3544bc7ed987915716ef6d0c07a775c4731d1e67
PhilipDeFraties <philip.defraties@gmail.com>
2024-12-06 12:37:29 -0700

    Centralize error handling for ActiveRecord::RecordNotFound in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found
+
+  private
+
+  def handle_not_found(exception)
+    render json: { error: exception.message }, status: :not_found
+  end
 end
