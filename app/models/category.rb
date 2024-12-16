2dc47d70d13bc85a5c2d215ebce3ee1bc482bee3
PhilipDeFraties <philip.defraties@gmail.com>
2024-12-16 14:51:53 -0700

    fix: update Category model and model spec for rubocop offenses

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,15 @@
 class Category < ApplicationRecord
-  has_many :subcategories, class_name: "Category", foreign_key: "parent_id", dependent: :destroy
-  belongs_to :parent_category, class_name: "Category", foreign_key: "parent_id", optional: true
+  has_many :subcategories,
+           class_name: "Category",
+           foreign_key: "parent_id",
+           dependent: :destroy,
+           inverse_of: :parent_category
 
-  has_and_belongs_to_many :products
+  belongs_to :parent_category,
+             class_name: "Category",
+             foreign_key: "parent_id",
+             optional: true,
+             inverse_of: :subcategories
 
   validates :name, presence: true, uniqueness: true
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   subject { described_class.new(name: "Electronics") }
-  it { should validate_presence_of(:name) }
-  it { should validate_uniqueness_of(:name) }
-  it { should belong_to(:parent_category).optional }
-  it { should have_many(:subcategories).dependent(:destroy) }
+
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_uniqueness_of(:name) }
+  it { is_expected.to belong_to(:parent_category).optional }
+  it { is_expected.to have_many(:subcategories).dependent(:destroy) }
 end
