2046e87733601f9931afb10e2cee3a415542f5fe
PhilipDeFraties <philip.defraties@gmail.com>
2025-01-03 10:13:58 -0700

    test: add model specs for CategoryProduct, Category, and Product associations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,5 +9,6 @@ RSpec.describe Product, type: :model do
     it { is_expected.to validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
     it { is_expected.to validate_presence_of(:stock_quantity) }
     it { is_expected.to validate_numericality_of(:stock_quantity).only_integer.is_greater_than_or_equal_to(0) }
+    it { is_expected.to have_many(:categories).through(:category_products) }
   end
 end
