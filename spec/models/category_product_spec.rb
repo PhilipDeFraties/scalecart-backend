e1224b5896881cf46be055c850770f1997b3e601
PhilipDeFraties <philip.defraties@gmail.com>
2025-01-03 10:20:11 -0700

    rubocop corrections

--- spec/models/category_product_spec.rb
+++ spec/models/category_product_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe CategoryProduct, type: :model do
+  subject { build(:category_product, category: category, product: product) }
+
   let(:category) { create(:category) }
   let(:product) { create(:product) }
-
-  subject { build(:category_product, category: category, product: product) }
 
   it { is_expected.to belong_to(:category) }
   it { is_expected.to belong_to(:product) }
 
   it "validates uniqueness of category_id scoped to product_id" do
     create(:category_product, category: category, product: product)
-    is_expected.to validate_uniqueness_of(:category_id).scoped_to(:product_id)
+    expect(subject).to validate_uniqueness_of(:category_id).scoped_to(:product_id)
   end
 end
