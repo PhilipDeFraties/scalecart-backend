c1306d17bb80b525083d575876332cb26623a640
PhilipDeFraties <philip.defraties@gmail.com>
2025-01-03 10:08:56 -0700

    feat(models): add associations for categories and products through join table

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
+  has_many :category_products, dependent: :destroy
+  has_many :products, through: :category_products
+
   has_many :subcategories,
            class_name: "Category",
            foreign_key: "parent_id",
