96da62dd99bf3682c0a167d791308b79a49c8ad3
PhilipDeFraties <philip.defraties@gmail.com>
2025-01-13 15:25:35 -0700

    feat: add expired_at column to jwt_denylists table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_13_203127) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_13_222435) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_13_203127) do
     t.datetime "exp", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "expired_at"
     t.index ["jti"], name: "index_jwt_denylists_on_jti", unique: true
   end
 
