7ca0b51939afb09cc2ef990745e7efa36b8319da
PhilipDeFraties <philip.defraties@gmail.com>
2025-01-13 13:12:06 -0700

    test: add model specs for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
+
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:user) }
+
+  describe "Validations" do
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    it { is_expected.to validate_presence_of(:password) }
+    it { is_expected.to validate_length_of(:password).is_at_least(6) }
+  end
+
+  describe "Database" do
+    it { is_expected.to have_db_column(:email).of_type(:string) }
+    it { is_expected.to have_db_column(:encrypted_password).of_type(:string) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
+  end
+
+  describe "Associations" do
+  end
+
+  describe "Roles" do
+    it "assigns a default role if not specified" do
+      user = User.new(email: "test@example.com", password: "password")
+      expect(user.role).to eq("customer")
+    end
+  end
 end
