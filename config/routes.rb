374e7d81249aa1d36a754c0b385c7431c3b8a1f0
PhilipDeFraties <philip.defraties@gmail.com>
2025-01-14 15:35:51 -0700

    chore: remove Devise configuration and initializers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
   resources :products, only: %i[index show create update destroy]
   resources :categories, only: %i[index show create update destroy]
 end
