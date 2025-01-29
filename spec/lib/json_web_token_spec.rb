2d57920198280e92a808edc68a7250562ee4e731
PhilipDeFraties <philip.defraties@gmail.com>
2025-01-29 16:21:44 -0700

    rubocop corrections

--- spec/lib/json_web_token_spec.rb
+++ spec/lib/json_web_token_spec.rb
@@ -3,7 +3,7 @@ require 'json_web_token'
 
 RSpec.describe JsonWebToken do
   let(:payload) { { user_id: 1 } }
-  let(:token) { JsonWebToken.encode(payload) }
+  let(:token) { described_class.encode(payload) }
 
   describe '.encode' do
     it 'returns a valid JWT token' do
@@ -14,18 +14,18 @@ RSpec.describe JsonWebToken do
 
   describe '.decode' do
     it 'decodes a valid token' do
-      decoded_payload = JsonWebToken.decode(token)
+      decoded_payload = described_class.decode(token)
       expect(decoded_payload['user_id']).to eq(payload[:user_id])
     end
 
     it 'returns nil for an invalid token' do
-      expect(JsonWebToken.decode('invalid_token')).to be_nil
+      expect(described_class.decode('invalid_token')).to be_nil
     end
 
     it 'returns nil for an expired token' do
-      expired_token = JsonWebToken.encode(payload, 1.second.ago)
+      expired_token = described_class.encode(payload, 1.second.ago)
       sleep 2
-      expect(JsonWebToken.decode(expired_token)).to be_nil
+      expect(described_class.decode(expired_token)).to be_nil
     end
   end
 end
