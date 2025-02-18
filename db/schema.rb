380c89aa31db9411b38d5743a54ec173ed2263ec
PhilipDeFraties <philip.defraties@gmail.com>
2025-02-17 17:42:40 -0700

    eat: Add users table migration and update schema
    - Create `users` table with `email` and `password_digest`
    - Add `role` column as an enum type (customer/admin)
    - Ensure `email` is unique and non-null
    - Update `schema.rb` to reflect migration changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_16_232255) do
+ActiveRecord::Schema[7.2].define(version: 2025_02_18_002227) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,15 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_16_232255) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_products_on_name", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.string "role", default: "customer", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "category_products", "categories"
