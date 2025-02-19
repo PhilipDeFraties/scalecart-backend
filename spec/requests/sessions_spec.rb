d64e037783dd50de341d962c301f26fd1beaa664
PhilipDeFraties <philip.defraties@gmail.com>
2025-02-18 20:21:22 -0700

    add spec for throttled response in session spec

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Authentication API', type: :request do
+  include ActiveSupport::Testing::TimeHelpers
   let(:user) { create(:user, password: 'password123') }
 
   describe 'POST /login' do
@@ -19,6 +20,27 @@ RSpec.describe 'Authentication API', type: :request do
 
         expect(response).to have_http_status(:unauthorized)
         expect(response.body).to include('Invalid email or password')
+      end
+    end
+
+    context 'when login attempts exceed the limit' do
+      it 'returns a too many requests error' do
+        headers = {"REMOTE_ADDR" => "1.2.3.4"}
+
+        5.times do
+          post '/login', params: { email: user.email, password: 'password123' }, headers: headers
+        end
+        post '/login', params: { email: user.email, password: 'password123' }, headers: headers
+
+        expect(response.body).to include('Too many login attempts. Please try again later.')
+        expect(response).to have_http_status(:too_many_requests)
+
+        travel_to(10.minutes.from_now) do
+          post '/login', params: { email: user.email, password: 'password123' }, headers: headers
+
+          expect(response).to have_http_status(:ok)
+          expect(session[:user_id]).to eq(user.id)
+        end
       end
     end
   end
